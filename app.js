0a134b39a207f311c94fd9d609db26a3db996e6d
DeveloFer77 <fernando.proff@hotmail.com>
2021-06-23 23:10:17 -0300

    Creamos config/yargs para limpiar app.js y le agregamos una descripcion a nuestras banderas

--- app.js
+++ app.js
@@ -1,37 +1,8 @@
 
 const {crearArchivo} = require('./helpers/multiplicar')
-
-const argv = require('yargs')
-                .options({
-                    'b': {
-                    alias: 'base',
-                    demandOption: true,
-                    type: 'number'
-                    },
-                    'l': {
-                        alias: 'listar',
-                        type: 'boolean',
-                        demandOption: true,
-                        default: false
-                    }
-                })
-                .check((argv, options) => {
-                    if( isNaN( argv.b )){
-                        throw 'La base tiene que ser un numero'
-                    }
-                    return true;
-                })
-                .argv
+const argv = require('./config/yargs') 
 
 console.clear()
-
-//console.log( 'base yargs:', argv.base)
-// default: '/etc/passwd',
-// describe: 'x marks the spot',
-
-
-
-// const base = 5;
 
 crearArchivo( argv.b, argv.l )
     .then( nombreArchivo => console.log(nombreArchivo, 'creado'))
